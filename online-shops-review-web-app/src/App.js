7e87d6570d11ef57b11ee35c0839cb3b5074fee6
marukosy124 <soniayeung12435@gmail.com>
2020-08-20 01:26:27 +0800

    fix sticky bar above content

--- online-shops-review-web-app/src/App.js
+++ online-shops-review-web-app/src/App.js
@@ -1,34 +1,30 @@
-import React from "react";
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import './assets/App.css';
-// import 'semantic-ui-less/semantic.less'
+import { Menu, Sticky } from 'semantic-ui-react'
 import Home from './screens/Home'
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <div class="ui top fixed menu">
+      <Sticky >
+        <Menu attached='top'>
           <Link to="/" class="item">Home</Link>
           <Link to="/about" class="item">About</Link>
-          <Link to="/users" class="item">Users</Link>
-        </div>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-          </Route>
-        </Switch>
-      </div>
+        </Menu>
+      </Sticky>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/users">
+        </Route>
+      </Switch>
     </Router>
-  );
+  )
 }
+
