b5a472ad0e261562ec09227bf597f0b64dce7d79
marukosy124 <soniayeung12435@gmail.com>
2020-08-22 01:07:46 +0800

    add search bar and revamp router

--- online-shops-review-web-app/src/screens/Home.js
+++ online-shops-review-web-app/src/screens/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import '../assets/App.css';
+import HomeSearchBar from "../components/HomeSearchBar";
 
 export default function Home() {
   return (
+    <>
     <h1 class="ui center aligned header">
       Online Shops Reviews
     </h1>
+    <HomeSearchBar />
+    </>
   );
 }
