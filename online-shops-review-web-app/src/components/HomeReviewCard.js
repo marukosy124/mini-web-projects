ba0953eb8df09fef97448f877fe5802955a6baf2
marukosy124 <soniayeung12435@gmail.com>
2020-08-27 00:35:47 +0800

    format date; add parser and clamp for rtf;

--- online-shops-review-web-app/src/components/HomeReviewCard.js
+++ online-shops-review-web-app/src/components/HomeReviewCard.js
@@ -2,28 +2,34 @@ import React from "react";
 import Moment from "react-moment";
 import "../assets/App.css";
 import { Card, Icon, Image } from "semantic-ui-react";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
 export default function HomeReviewCard({ reviewContent }) {
   return (
+    // TODO: adjust card margins
     <Card>
       <Card.Content extra>
         <Icon name="user" />
         {reviewContent.authorID}
         <Card.Meta>
-          {/* TODO: format date */}
-          {/* {reviewContent.publishedDate.toDate().format('YYYY/MM/DD')} */}
-          <Moment toNow>1976-04-19T12:59-0500</Moment>
+          <Moment fromNow>{reviewContent.publishedDate.toDate()}</Moment>
         </Card.Meta>
       </Card.Content>
-      <Image
+      {/* <Image
         src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
         wrapped
         ui={false}
-      />
+      /> */}
       <Card.Content>
         <Card.Header>{reviewContent.title}</Card.Header>
-        {/* TODO: convert rtf to plain text */}
-        <Card.Description>{reviewContent.content}</Card.Description>
+        <Card.Description>
+          <HTMLEllipsis
+            unsafeHTML={reviewContent.content}
+            maxLine="5"
+            ellipsis="..."
+            basedOn="letters"
+          />
+        </Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Icon name="thumbs up outline" />
