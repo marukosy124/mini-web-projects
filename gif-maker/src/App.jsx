359fa87c42684e25332c440b23a4e1bade4e8150
marukosy124 <soniayeung12435@gmail.com>
2021-04-01 02:59:26 +0800

    add start time end time

--- gif-maker/src/App.jsx
+++ gif-maker/src/App.jsx
@@ -10,81 +10,112 @@ function App() {
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
   const [gif, setGif] = useState();
+  const [timeslot, setTimeslot] = useState({
+    startTime: '00:00:00',
+    endTime: '00:00:05',
+  });
 
   const load = async () => {
     await ffmpeg.load();
     setReady(true);
-  }
+  };
 
   useEffect(() => {
     load();
-  }, [])
+  }, []);
+
+  const getVideoURL = (blob) => {
+    if (!blob.url) {
+      blob.url = URL.createObjectURL(blob);
+    }
+    return blob.url;
+  };
+
+  const onTimeChange = (e) => {
+    setTimeslot({
+      ...timeslot,
+      [e.target.id]: e.target.value,
+    });
+  };
 
   const convertToGif = async () => {
-    // Write the file to memory 
+    // Write the file to memory
     ffmpeg.FS('writeFile', 'temp.mp4', await fetchFile(video));
 
     // Run the FFMpeg command
-    await ffmpeg.run('-i', 'temp.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'output.gif');
+    await ffmpeg.run(
+      '-i',
+      'temp.mp4',
+      '-t',
+      timeslot.endTime,
+      '-ss',
+      timeslot.startTime,
+      '-f',
+      'gif',
+      'output.gif',
+    );
 
     // Read the result
     const data = ffmpeg.FS('readFile', 'output.gif');
 
     // Create a URL
-    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
-    setGif(url)
-  }
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'image/gif' }),
+    );
+    setGif(url);
+  };
 
   const saveGif = async () => {
     axios({
-      method: "get",
+      method: 'get',
       url: gif,
-      responseType: "arraybuffer"
+      responseType: 'arraybuffer',
     })
       .then((response) => {
-        var link = document.createElement("a");
-        link.href = window.URL.createObjectURL(
-          new Blob([response.data], { type: "application/octet-stream" })
+        var link = document.createElement('a');
+        link.href = URL.createObjectURL(
+          new Blob([response.data], { type: 'application/octet-stream' }),
         );
-        link.download = "output.gif";
-    
+        link.download = 'output.gif';
+
         document.body.appendChild(link);
-    
+
         link.click();
         setTimeout(function () {
-          window.URL.revokeObjectURL(link);
+          URL.revokeObjectURL(link);
         }, 200);
       })
       .catch((error) => console.log(error));
-  }
+  };
 
   return ready ? (
-    
     <div className="App">
-      { video && <video
-        controls
-        width="250"
-        src={URL.createObjectURL(video)}>
-
-      </video>}
-
-
+      {video && <video controls width="400" src={getVideoURL(video)}></video>}
+      <div>
+        Start time:&nbsp;
+        <input
+          type="text"
+          id="startTime"
+          placeholder="00:00:00"
+          onChange={onTimeChange}
+        />
+        &nbsp;End time:&nbsp;
+        <input
+          type="text"
+          id="endTime"
+          placeholder="00:00:00"
+          onChange={onTimeChange}
+        />
+      </div>
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
-
       <button onClick={convertToGif}>Convert</button>
-
       <h3>Result</h3>
-
-      { gif && <img src={gif} width="250" />}
-
+      {gif && <img src={gif} width="400" />}
       <button onClick={saveGif}>Save</button>
-
     </div>
-  )
-    :
-    (
-      <p>Loading...</p>
-    );
+  ) : (
+    <p>Loading...</p>
+  );
 }
 
 export default App;
